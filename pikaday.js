360031baf0bd92abfeb1217015af0ea58dc0d218
Ozgur Adem Isikli <oai@proreact.dk>
2024-08-08 10:20:44 +0200

    Fixed initialization issues

--- pikaday.js
+++ pikaday.js
@@ -9,8 +9,9 @@
     'use strict';
 
     var moment;
-    if (window.moment !== undefined) {
+    if (window.moment) {
         moment = window.moment
+        module.exports = factory(moment);
     } else if (typeof exports === 'object') {
         // CommonJS module
         // Load moment.js as an optional dependency
